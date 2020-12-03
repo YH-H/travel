86717fca7fce97e639b37f4ea1d98f376841d0f1
YH-H <54827771+YH-H@users.noreply.github.com>
2020-12-03 15:55:48 +0800

    Create CategoryServiceImpl.java

---
+++ src/main/java/cn/itcast/travel/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package cn.itcast.travel.service.impl;
+
+import cn.itcast.travel.dao.CategoryDao;
+import cn.itcast.travel.dao.impl.CategoryDaoImpl;
+import cn.itcast.travel.domain.Category;
+import cn.itcast.travel.service.CategoryService;
+import cn.itcast.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryDao categoryDao = new CategoryDaoImpl();
+
+    @Override
+    public List<Category> findAll() {
+        // 为什么不直接调用dao层的findAll
+        return categoryDao.findAll();
+    }
+}
